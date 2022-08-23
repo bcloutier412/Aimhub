6d2a6b8187f703cf8cafbd76172c85c21db3a29d
Dino <brandoncloutier412@gmail.com>
2022-08-23 15:42:54 -0700

    full redesign of the gamescontainer

--- src/GameLauncher/Games/Games.component.jsx
+++ src/GameLauncher/Games/Games.component.jsx
@@ -1,6 +1,6 @@
 import styles from "./Games.module.css";
-// import { NavLink } from "react-router-dom";
-// import playBtn from "../assets/playbtn.png";
+import { NavLink } from "react-router-dom";
+import playBtn from "../assets/playbtn.png";
 import { useState } from "react";
 
 //Game Assets
@@ -17,16 +17,19 @@ const games = [
     name: "Grid Shot",
     route: "../GridShot",
     gameImage: aimlabsScreenshot,
+    attributes: ["speed", "mouse control", "consistency"],
   },
   {
     id: 1,
     name: "Reaction Trainer",
     route: "../ReactionTrainer",
+    attributes: ["speed", "reaction time"],
   },
   {
     id: 2,
-    name: "Relex Trainer",
+    name: "Reflex Trainer",
     route: "../ReactionTrainer",
+    attributes: ["speed", "reflexes", "muscle memory", "consistency"],
   },
 ];
 
@@ -37,40 +40,73 @@ const games = [
     Each game widet will have a button to change the react route to 
     start the game (Changing the main window to the Game)
 */
-// <NavLink to={game.route} style={{ textDecoration: "none" }}>
-//                   <div className={styles.button}>
-//                     <img alt='play button' src={playBtn} />
-//                     <span className={styles.playText}>PLAY</span>
-//                   </div>
-//                 </NavLink>
 export default function Games() {
   const [currentGame, changeCurrentGame] = useState(0);
+
   return (
     <>
       <div
-        className={styles.gameInfoContainer}
+        className={styles.mainContainer}
         style={{ backgroundImage: `url(${games[currentGame].gameImage})` }}
       >
-        <div className={styles.gameInfo}></div>
-      </div>
-      <div className={styles.games}>
-        <div className={styles.header}>GAMES</div>
-        <div className={styles.gameWidgets}>
-          {games.map((game) => {
-            return (
-              <div
-                className={styles.gameWidget}
-                key={game.id}
-                onClick={() => changeCurrentGame(game.id)}
+        <div className={styles.gamesWrapper}>
+          <div className={styles.gameInfo}>
+            <div className={styles.gameTitle}>
+              <h1>{games[currentGame].name}</h1>
+              <ul className={styles.gameAttributes}>
+                {games[currentGame].attributes.map((attribute) => (
+                  <li
+                    key={attribute.toString()}
+                    className={styles.gameAttribute}
+                  >
+                    {attribute}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className={styles.gameDescription}>
+              <h3>Description</h3>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+              
+              </p>
+            </div>
+            <div className={styles.playBtn}>
+              <NavLink
+                to={games[currentGame].route}
+                style={{ textDecoration: "none" }}
               >
-                <div className={styles.gameWidgetImage}></div>
-                <div className={styles.gameWidgetText}>
-                  <h4>2022</h4>
-                  <h3>{game.name}</h3>
+                <div className={styles.button}>
+                  <img alt="play button" src={playBtn} />
+                  <span className={styles.playText}>PLAY</span>
                 </div>
-              </div>
-            );
-          })}
+              </NavLink>
+            </div>
+          </div>
+          <div className={styles.games}>
+            <div className={styles.header}>GAMES</div>
+            <div className={styles.gameWidgets}>
+              {games.map((game) => {
+                return (
+                  <div
+                    className={`${styles.gameWidget} ${
+                      currentGame === game.id ? styles.active : "null"
+                    }`}
+                    key={game.id}
+                    onClick={() => changeCurrentGame(game.id)}
+                  >
+                    <div className={styles.gameWidgetImage}></div>
+                    <div className={styles.gameWidgetText}>
+                      <h4>2022</h4>
+                      <h3>{game.name}</h3>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </>
