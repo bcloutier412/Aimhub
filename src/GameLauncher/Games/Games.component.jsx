610915c541f0900a1a8e5a8d339a2d7ac862d7ea
Dino <brandoncloutier412@gmail.com>
2023-01-04 10:27:57 -0800

    added widget images

--- src/GameLauncher/Games/Games.component.jsx
+++ src/GameLauncher/Games/Games.component.jsx
@@ -17,6 +17,7 @@ const games = [
     name: "Grid Shot",
     route: "../GridShot",
     gameImage: aimlabsScreenshot,
+    gameWidgetImage: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT930tN61QpBIzAY7Xti5_kRUV_b-aY5Asex64w6Cz4Gem3XrM5blzi_yCaaHR26pMtbM&usqp=CAU)",
     attributes: ["speed", "percision", "consistency"],
     description:
       "Grid Shot will develop your speed, mouse control, and percision. Seven targets will spawn randomly across an invisible grid on the screen. Kill one and another spawns so there are always seven targets on the screen",
@@ -25,6 +26,7 @@ const games = [
     id: 1,
     name: "Reaction Trainer",
     route: "../ReactionTrainer",
+    gameWidgetImage: "url(https://www.creativefabrica.com/wp-content/uploads/2018/11/Clock-timer-logo-by-Mansel-Brist-2-580x406.jpg)",
     attributes: ["speed", "reaction time"],
     description: "Coming soon"
   },
@@ -32,6 +34,7 @@ const games = [
     id: 2,
     name: "Reflex Trainer",
     route: "../ReactionTrainer",
+    gameWidgetImage: "url(https://previews.123rf.com/images/bsd555/bsd5552002/bsd555200201541/140704064-scrolling-down-computer-mouse-white-rgb-color-icon-internet-page-browsing-arrow-scrolldown-gesture-i.jpg)",
     attributes: [ "muscle memory", "consistency"],
     description: "Coming Soon"
   },
@@ -98,7 +101,7 @@ export default function Games() {
                     key={game.id}
                     onClick={() => changeCurrentGame(game.id)}
                   >
-                    <div className={styles.gameWidgetImage}></div>
+                    <div className={styles.gameWidgetImage} style={{backgroundImage: `${game.gameWidgetImage}`}}></div>
                     <div className={styles.gameWidgetText}>
                       <h4>2022</h4>
                       <h3>{game.name}</h3>
